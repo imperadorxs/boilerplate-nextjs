60d464f9d9ad759b7e3e863f7eec371db52465c4
Allan Santiago <allan75imp@gmail.com>
2022-05-15 14:19:51 -0300

    chore: configure jest react-testing-library

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+};
