a3cd148d07e63f8d294b4ec249f5c5cac40013b8
Allan Santiago <allan75imp@gmail.com>
2022-05-28 13:14:50 -0300

    chore: add pwa in this project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa');
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Template Typescript</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
